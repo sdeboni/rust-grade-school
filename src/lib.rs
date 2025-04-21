ce5a8a2f13853036111beed6d1b3d6c29051140e
Sam De Boni <sdeboni@gmail.com>
2025-04-21 15:06:55 -0400

    use BTree to keep keys and set in sort order

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 
 pub struct School {
-    students_by_grade: HashMap<u32, HashSet<String>>,
+    students_by_grade: BTreeMap<u32, BTreeSet<String>>,
 }
 
 impl School {
     pub fn new() -> School {
         School {
-            students_by_grade: HashMap::new(),
+            students_by_grade: BTreeMap::new(),
         }
     }
 
@@ -26,9 +26,7 @@ impl School {
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        let mut grades: Vec<u32> = self.students_by_grade.keys().copied().collect();
-        grades.sort();
-        grades
+        self.students_by_grade.keys().copied().collect()
     }
 
     // If `grade` returned a reference, `School` would be forced to keep a `Vec<String>`
@@ -36,11 +34,9 @@ impl School {
     // the internal structure can be completely arbitrary. The tradeoff is that some data
     // must be copied each time `grade` is called.
     pub fn grade(&self, grade: u32) -> Vec<String> {
-        let mut ret = match self.students_by_grade.get(&grade) {
+        match self.students_by_grade.get(&grade) {
             Some(students) => students.iter().map(|s| s.to_string()).collect(),
             None => vec![],
-        };
-        ret.sort();
-        ret
+        }
     }
 }
